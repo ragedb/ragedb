c77bc469ec825843d74a48892e325cfb1e867768
Max De Marzi <maxdemarzi@hotmail.com>
2021-08-05 22:53:33 -0500

    split build and coverage github actions

--- cmake/Sanitizers.cmake
+++ cmake/Sanitizers.cmake
@@ -1,9 +1,8 @@
 function(enable_sanitizers project_name)
 
   if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
-    option(ENABLE_COVERAGE "Enable coverage reporting for gcc/clang" FALSE)
-
-    if(ENABLE_COVERAGE)
+    if(CMAKE_BUILD_TYPE STREQUAL "Debug")
+      set(ENABLE_COVERAGE ON CACHE BOOL "Enable coverage reporting for gcc/clang")
       target_compile_options(${project_name} INTERFACE --coverage -O0 -g)
       target_link_libraries(${project_name} INTERFACE --coverage)
     endif()
