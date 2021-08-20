b99273e12d77438fbbb965f254acdf16eee61851
Max De Marzi <maxdemarzi@hotmail.com>
2021-08-20 00:33:01 -0500

    simplify find

--- src/graph/peered/Find.cpp
+++ src/graph/peered/Find.cpp
@@ -40,53 +40,33 @@ namespace ragedb {
     seastar::future<std::vector<uint64_t>> Shard::FindNodeIdsPeered(uint16_t node_type_id, const std::string& property, const Operation& operation, const std::any& value, uint64_t skip, uint64_t limit) {
         uint64_t max = skip + limit;
 
-        // Get the {Node Type Id, Count} map for each core
-        std::vector<seastar::future<uint64_t>> futures;
-        for (int i=0; i<cpus; i++) {
-            auto future = container().invoke_on(i, [node_type_id, property, operation, value] (Shard &local_shard) mutable {
-                return local_shard.FindNodeCount(node_type_id, property, operation, value);
+        std::vector<seastar::future<std::vector<uint64_t>>> futures;
+
+        for (int i=0; i < cpus; i++) {
+            auto future = container().invoke_on(i, [node_type_id, property, operation, value, max] (Shard &local_shard) mutable {
+                return local_shard.FindNodeIds(node_type_id, property, operation, value, 0, max);
             });
             futures.push_back(std::move(future));
         }
 
         auto p = make_shared(std::move(futures));
-        return seastar::when_all_succeed(p->begin(), p->end()).then([p, node_type_id, property, operation, value, skip, max, limit, this] (const std::vector<uint64_t>& results) {
+        return seastar::when_all_succeed(p->begin(), p->end()).then([p, node_type_id, property, operation, value, skip, limit, max, this] (const std::vector<std::vector<uint64_t>>& results) {
             uint64_t current = 0;
-            uint64_t next = 0;
-            int current_shard_id = 0;
 
-            std::map<uint16_t, std::pair<uint64_t , uint64_t>> requests;
-            for (const auto& count : results) {
-                next = current + count;
-                if (next > skip) {
-                    std::pair<uint64_t, uint64_t> pair = std::make_pair((skip > current) ? skip - current : 0, limit);
-                    requests.insert({ current_shard_id, pair });
-                    if (next <= max) {
-                        break; // We have everything we need
+            std::vector<uint64_t> ids;
+            ids.reserve(limit);
+
+            for (auto result : results) {
+                for( auto id : result) {
+                    if (++current > skip) {
+                        ids.push_back(id);
+                    }
+                    if (current >= limit) {
+                        return ids;
                     }
                 }
-                current = next;
-                current_shard_id++;
             }
-
-            std::vector<seastar::future<std::vector<uint64_t>>> futures;
-
-            for (const auto& request : requests) {
-                auto future = container().invoke_on(request.first, [node_type_id, property, operation, value, request] (Shard &local_shard) mutable {
-                    return local_shard.FindNodeIds(node_type_id, property, operation, value, request.second.first, request.second.second);
-                });
-                futures.push_back(std::move(future));
-            }
-
-            auto p2 = make_shared(std::move(futures));
-            return seastar::when_all_succeed(p2->begin(), p2->end()).then([p2, limit] (const std::vector<std::vector<uint64_t>>& results) {
-                std::vector<uint64_t> ids;
-                ids.reserve(limit);
-                for (auto result : results) {
-                    ids.insert(std::end(ids), std::begin(result), std::end(result));
-                }
-                return ids;
-            });
+            return ids;
         });
     }
 
@@ -98,58 +78,33 @@ namespace ragedb {
     seastar::future<std::vector<Node>> Shard::FindNodesPeered(uint16_t node_type_id, const std::string& property, const Operation& operation, const std::any& value, uint64_t skip, uint64_t limit) {
         uint64_t max = skip + limit;
 
-        // Get the {Node Type Id, Count} map for each core
-        std::vector<seastar::future<uint64_t>> futures;
+        std::vector<seastar::future<std::vector<Node>>> futures;
         for (int i=0; i < cpus; i++) {
-            auto future = container().invoke_on(i, [node_type_id, property, operation, value] (Shard &local_shard) mutable {
-                return local_shard.FindNodeCount(node_type_id, property, operation, value);
+            auto future = container().invoke_on(i, [node_type_id, property, operation, value, max] (Shard &local_shard) mutable {
+                return local_shard.FindNodes(node_type_id, property, operation, value, 0, max);
             });
             futures.push_back(std::move(future));
         }
 
         auto p = make_shared(std::move(futures));
-        return seastar::when_all_succeed(p->begin(), p->end()).then([p, node_type_id, property, operation, value, skip, max, limit, this] (const std::vector<uint64_t>& results) {
+        return seastar::when_all_succeed(p->begin(), p->end()).then([p, node_type_id, property, operation, value, skip, limit, max, this] (const std::vector<std::vector<Node>>& results) {
             uint64_t current = 0;
-            uint64_t next = 0;
-            int current_shard_id = 0;
 
-            std::map<uint16_t, std::pair<uint64_t , uint64_t>> requests;
-            for (const auto& count : results) {
-                // If we have no results on this shard, skip it
-                if (count == 0) {
-                    current_shard_id++;
-                    continue;
-                }
-                next = current + count;
-                if (next > skip) {
-                    std::pair<uint64_t, uint64_t> pair = std::make_pair((skip > current) ? skip - current : 0, limit - current);
-                    requests.insert({ current_shard_id, pair });
-                    if (next > max) {
-                        break; // We have everything we need
+            std::vector<Node> nodes;
+            nodes.reserve(limit);
+
+            for (auto result : results) {
+                for( auto node : result) {
+                    if (++current > skip) {
+                        nodes.push_back(node);
+                    }
+                    if (current >= limit) {
+                        return nodes;
                     }
                 }
-                current = next;
-                current_shard_id++;
             }
-
-            std::vector<seastar::future<std::vector<Node>>> futures;
-
-            for (const auto& request : requests) {
-                auto future = container().invoke_on(request.first, [node_type_id, property, operation, value, request] (Shard &local_shard) mutable {
-                    return local_shard.FindNodes(node_type_id, property, operation, value, request.second.first, request.second.second);
-                });
-                futures.push_back(std::move(future));
-            }
-
-            auto p2 = make_shared(std::move(futures));
-            return seastar::when_all_succeed(p2->begin(), p2->end()).then([p2, limit] (const std::vector<std::vector<Node>>& results) {
-                std::vector<Node> nodes;
-                nodes.reserve(limit);
-                for (auto result : results) {
-                    nodes.insert(std::end(nodes), std::begin(result), std::end(result));
-                }
-                return nodes;
-            });
+            return nodes;
         });
     }
+
 }
