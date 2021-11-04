57aece61195b3627b11d0b85e31936aa945062e9
Max De Marzi <maxdemarzi@hotmail.com>
2021-11-04 00:42:25 -0500

    getting a response

--- public/js/ragedb.js
+++ public/js/ragedb.js
@@ -7,3 +7,27 @@ editor.session.setMode(new LuaHighlightRules());
 function addtext(newtext) {
     editor.insert(newtext);
 }
+
+
+function sendscript() {
+    const url = "/db/rage/lua";
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", url);
+
+    xhr.setRequestHeader("Accept", "application/json");
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            console.log(xhr.status);
+            console.log(xhr.responseText);
+
+            let ele = document.getElementById('response');
+            ele.innerHTML = xhr.responseText;
+        }};
+
+    xhr.send( editor.getValue());
+}
+
+function clearscript() {
+    editor.setValue("");
+}
