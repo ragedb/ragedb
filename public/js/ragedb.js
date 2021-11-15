265a082a38bf8659beac953fdd8d0f683758c7f9
Max De Marzi <maxdemarzi@hotmail.com>
2021-11-15 03:04:07 -0600

    hard fought javascript magic

--- public/js/ragedb.js
+++ public/js/ragedb.js
@@ -28,9 +28,135 @@ function sendscript() {
             ele.innerHTML = xhr.responseText;
         }};
 
+    let query = editor.getValue();
+
+    if (query) {
+       addQueryToHistory(query);
+    }
+
     xhr.send( editor.getValue());
 }
 
-function clearscript() {
-    editor.setValue("");
+function clearEditor() {
+    editor.clear();
 }
+
+function setEditor(query) {
+    editor.setValue(query);
+}
+
+
+// Mail
+async function getMail() {
+    let url = 'https://ragedb.com/inbox.json';
+    try {
+        let res = await fetch(url);
+        return await res.json();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function renderMail() {
+    let messages = await getMail();
+    let html = '';
+    messages.forEach(message => {
+        let htmlSegment = `<div class="icon-text">
+                            <span class="icon has-text-info has-tooltip-arrow has-tooltip-right" data-tooltip="${message.description}">
+                                <i class="fas fa-info-circle"></i>
+                            </span>
+                            <h2 class="subtitle">${message.title}</h2>
+                            </div>`;
+
+        html += htmlSegment;
+    });
+
+    let container = document.getElementById("inbox");
+    container.innerHTML = html;
+    let collapsible = document.getElementById("collapsible-inbox");
+    collapsible.style.height = "100%";
+
+}
+
+renderMail();
+
+// History
+let db;
+
+initIndexedDB();
+
+async function initIndexedDB() {
+
+    db = await idb.openDB('ragedb', 1, {
+        upgrade(db) {
+            // Create a store of objects
+            const store = db.createObjectStore('history', {
+                // The 'id' property of the object will be the key.
+                keyPath: 'id',
+                // If it isn't explicitly set, create a value by auto incrementing.
+                autoIncrement: true,
+            });
+
+            //store.createIndex('unique_history', 'text',  { unique: true })
+          },
+    });
+
+    // await listHistory();
+}
+
+async function listHistory() {
+    let tx = db.transaction("history");
+    let historyStore = tx.objectStore("history");
+    let history = await historyStore.getAll();
+
+    let container = document.getElementById("history");
+    if (history.length) {
+        container.innerHTML = history.slice(0).reverse().map(query =>
+            `<span id="history-${query.id}" class="icon-text has-tooltip-arrow has-tooltip-bottom" onclick="setEditor(this.dataset.tooltip)">
+                 <span class="icon has-text-info">
+                    <i class="fas fa-angle-right"></i>
+                </span>
+                <p class="is-size-7">${query.text.substring(0, 10)}</p>
+            </span>`).join("<br/> ");
+
+        history.map(query => {
+            document.getElementById("history-" + query.id).dataset.tooltip = query.text;
+        })
+    } else {
+        container.innerHTML = `
+            <span class="icon-text has-tooltip-arrow has-tooltip-bottom" data-tooltip="Try running a query!">
+                <span class="icon has-text-info">
+                    <i class="fas fa-angle-right"></i>
+                </span>
+                <p class="is-size-7">No Queries have run</p>
+            <span/>`;
+
+    }
+
+    let collapsible = document.getElementById("collapsible-history");
+    collapsible.style.height = "100%";
+}
+
+async function clearHistory() {
+    let tx = db.transaction("history", "readwrite");
+    await tx.objectStore("history").clear();
+    await listHistory();
+}
+
+// Let's only keep 10 historic queries
+async function addQueryToHistory(query) {
+    let tx = db.transaction("history", "readwrite");
+    let historyStore = tx.objectStore("history");
+    let queries = await historyStore.getAll();
+
+    if (queries.length > 10) {
+        let id = queries[0].id;
+        historyStore.delete(id);
+    }
+   await tx.objectStore("history").add({text: query});
+   await listHistory();
+ }
+
+window.addEventListener('unhandledrejection', event => {
+    console.log("Error: " + event.reason.message);
+});
