1b6bd1e14e21330176886b640fad2c167d7a3e94
Max De Marzi <maxdemarzi@hotmail.com>
2022-01-31 13:22:08 -0600

    add local to speed up example bulk query

--- examples/50m_relationship_traversal_bulk.lua
+++ examples/50m_relationship_traversal_bulk.lua
@@ -1,11 +1,11 @@
-count = 0
-person = "person"..math.random(NodeTypesGetCountByType("Person"))
-person_id = NodeGetId("Person", person)
-liked_items = NodeGetRelationshipsIdsByIdForDirectionForType(person_id, Direction.OUT, "LIKES")
-item_likes = LinksGetRelationshipsIds(liked_items, Direction.IN, "LIKES")
+local count = 0
+local person = "person"..math.random(NodeTypesGetCountByType("Person"))
+local person_id = NodeGetId("Person", person)
+local liked_items = NodeGetRelationshipsIdsByIdForDirectionForType(person_id, Direction.OUT, "LIKES")
+local item_likes = LinksGetRelationshipsIds(liked_items, Direction.IN, "LIKES")
 
 for item, likes in pairs(item_likes) do
-  other_person_likes = LinksGetRelationshipsIdsForDirectionForType(likes, Direction.OUT, "LIKES")
+  local other_person_likes = LinksGetRelationshipsIdsForDirectionForType(likes, Direction.OUT, "LIKES")
   for other_person, likes2 in pairs(other_person_likes) do
     for iter = 1,#likes2 do
         count = count + 1
