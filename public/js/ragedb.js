dfbfdb29aadf3fdd7b7552c74031641adf56c249
Max De Marzi <maxdemarzi@hotmail.com>
2022-03-12 23:28:07 -0600

    use let not var

--- public/js/ragedb.js
+++ public/js/ragedb.js
@@ -308,7 +308,7 @@ async function sendscript() {
 function syntaxHighlight(json) {
     json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
     return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
-        var cls = 'json-number';
+        let cls = 'json-number';
         if (/^"/.test(match)) {
             if (/:$/.test(match)) {
                 cls = 'json-key';
